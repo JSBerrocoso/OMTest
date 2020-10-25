dfb974e941298ad2aa91baa776af8ad77feeb3ef
Santiago Berrocoso <jsbs87@gmail.com>
2020-10-25 22:46:01 +0100

    New: Created viewmodels

---
+++ app/src/main/java/com/jsbs87/android/omtest/app/presentation/ui/favorites/FavoritesViewModel.kt
@@ -0,0 +1,7 @@
+package com.jsbs87.android.omtest.app.presentation.ui.favorites
+
+import androidx.lifecycle.ViewModel
+
+class FavoritesViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/jsbs87/android/omtest/app/presentation/ui/films/FilmsViewModel.kt
@@ -0,0 +1,11 @@
+package com.jsbs87.android.omtest.app.presentation.ui.films
+
+import com.jsbs87.android.omtest.app.domain.interactors.GetFilmsUseCase
+import com.jsbs87.android.omtest.app.presentation.platform.BaseViewModel
+
+class FilmsViewModel(private val getFilms: GetFilmsUseCase) : BaseViewModel() {
+
+    fun loadFilms() {
+        // TODO
+    }
+}
